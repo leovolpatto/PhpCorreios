548112621f2fbd942c8b2cd188cc2324218b1ba0
mateus <mateus@eccosys-02>
2014-05-31 11:23:33 -0300

    Implementação do modelo

---
+++ Proxy/Correios/Rastreamento/RastreamentoResponse/RastreamentoDados.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace PhpCorreios\Proxy\Correios\Rastreamento\RastreamentoResponse;
+
+final class RastreamentoDados {
+
+    public $_id;
+    public $tipo;
+    public $sto;
+    public $data;
+    public $hora;
+    public $descricao;
+    public $recebedor;
+    public $documento;
+    public $comentario;
+    public $local;
+    public $codigo;
+    public $cidade;
+    public $uf;
+    public $status;
+
+}
--- Proxy/Correios/Rastreamento/RastreamentoResponse/RastreamentoResult.php
+++ Proxy/Correios/Rastreamento/RastreamentoResponse/RastreamentoResult.php
@@ -2,8 +2,15 @@
 
 namespace PhpCorreios\Proxy\Correios\Rastreamento\RastreamentoResponse;
 
+use PhpCorreios\Proxy\Correios\Rastreamento\RastreamentoResponse\RastreamentoDados;
+
 final class RastreamentoResult {
     
+    public $rastreamentoDados = array();
+    
+    public function addRastreamentoDados(RastreamentoDados $dados){
+        array_push($this->rastreamentoDados, $dados);
+    }
     
     
     
